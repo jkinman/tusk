61ed399f91cf640c65bde558bf8b9dd69cac7458
Joel Kinman <joel.kinman@gmail.com>
2015-08-26 22:58:51 -0700

    initial commit of angular generator

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tuskApp
+ * @description
+ * # tuskApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tuskApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tuskApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tuskApp
+ */
+angular.module('tuskApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
